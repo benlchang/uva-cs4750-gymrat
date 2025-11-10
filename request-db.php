853b6f53289c0b11dfd937290308c47f63246196
Alston Hou <157038872+Shaowyu@users.noreply.github.com>
2025-11-10 15:50:16 -0500

    Update request-db.php

--- request-db.php
+++ request-db.php
@@ -69,14 +69,6 @@ function createAccount($computingId, $password, $f_name, $l_name, $year)
         $stmt->bindParam(':year', $year);
 
         $stmt->execute();
-        // good way
-        // $statement = $db->prepare($query);
-        // $statement->bindValue(':reqDate', $reqDate);
-        // $statement->bindValue(':roomNumber', $roomNumber);
-        // $statement->bindValue(':reqBy', $reqBy);
-        // $statement->bindValue(':repairDesc', $repairDesc);
-        // $statement->bindValue(':reqPriority', $reqPriority);
-        // $statement->execute();
 
         $rowsInserted = $stmt->rowCount();
 
@@ -85,17 +77,12 @@ function createAccount($computingId, $password, $f_name, $l_name, $year)
         if ($rowsInserted > 0) {
             return 1;
         }
-        // most likely, there should not be a problem adding a request since 
-        // a primary key of the table is auto_increment
-        // if ($statement->rowCount() == 0)
-        //     echo "Failed to add a request <br/>";
     }
     catch (PDOException $e) 
     {
         echo $e->getMessage();
 
-        // if there is a specific SQL-related error message
-        //    echo "generic message (don't reveal SQL-specific message)";
+      
     }
 }
 
